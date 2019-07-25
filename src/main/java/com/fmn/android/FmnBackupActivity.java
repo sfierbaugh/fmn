6a02e16ac72ecf8fe825ee2436f921b0098c502a
sfierbaugh <stephen@fierbaugh.org>
2019-07-25 09:52:21 +0000

    Can't find R.

--- src/main/java/com/fmn/android/FmnBackupActivity.java
+++ src/main/java/com/fmn/android/FmnBackupActivity.java
@@ -22,14 +22,15 @@ public class FmnBackupActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fmn_backup_activity_layout);
         Log.d("FmnBackupActivity", "onCreate");        
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(com.codenvy.template.android.R.menu.main, menu);
+        //getMenuInflater().inflate(com.codenvy.template.android.R.menu.main, menu);
+        getMenuInflater().inflate(com.fmn.android.R.menu.main, menu);
         return true;
     }
 
