570b642bdbaa7722e6e52dcd6860c916eb1b2e14
sfierbaugh <stephen@fierbaugh.org>
2019-07-28 19:53:20 +0000

    Working

--- src/main/java/com/fmn/android/FmnMainActivity.java
+++ src/main/java/com/fmn/android/FmnMainActivity.java
@@ -87,7 +87,7 @@ public class FmnMainActivity extends Activity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(com.fmn.android.R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
